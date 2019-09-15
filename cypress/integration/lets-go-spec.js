774882e64d94a237e91a5d00bc210229ceac19b9
Xiang Fan <xiangfan1992@gmail.com>
2019-09-15 10:26:17 -0400

    add test scenario: performs a speed test and then rate ISP

--- cypress/integration/lets-go-spec.js
+++ cypress/integration/lets-go-spec.js
@@ -2,11 +2,20 @@ const tests = require('../support/lets-go')
 
 describe('Lets Begin', function() {
 
-    it('Open browser to speedtest.net', function() {
-      tests.beginTests()
-    })
+    // it('Open browser to speedtest.net', function() {
+    //   tests.beginTests()
+    // })
 
     // Performs a speed test
+    it('Performs a speed test and then rate ISP', function() {
+      tests.beginTests();
+      tests.clickGoButton();
+      tests.shouldHavePing();
+      tests.shouldHaveDownloadSpeed();
+      tests.shouldHaveUploadSpeed();
+      tests.rateISP(4);
+    });
+
 
     // Verifies navigation etc...
 
--- cypress/support/lets-go.js
+++ cypress/support/lets-go.js
@@ -1,7 +1,45 @@
+const SPEED_TEST_WAIT_TIME = 60 * 1000; // 60s
+
 const beginTests = () => {
     cy.visit('https://www.speedtest.net')
 }
 
- module.exports = {
-     beginTests
- }
+const clickGoButton = () => {
+    cy.get('.start-button a.js-start-test').click();
+}
+
+const shouldHaveDownloadSpeed = () => {
+    // wait until it finds the download speed element with number in it
+    // timeout after 60s
+    cy.get('.result-item-download span.download-speed').contains(/\d/, {
+        timeout: SPEED_TEST_WAIT_TIME
+    }).should('be.visible');
+}
+
+const shouldHavePing = () => {
+    cy.get('.result-item-ping span.ping-speed').contains(/\d/, {
+        timeout: SPEED_TEST_WAIT_TIME
+    }).should('be.visible');
+}
+
+const shouldHaveUploadSpeed = () => {
+    cy.get('.result-item-upload span.upload-speed').contains(/\d/, {
+        timeout: SPEED_TEST_WAIT_TIME
+    }).should('be.visible');
+}
+
+const rateISP = (rating) => {
+    // rate ISP stars by rating number (1 - 5)
+    cy.get(`.result-item-isp .rating-container span:nth-child(${rating})`).click();
+    // make sure the star color changed after rating
+    cy.get('.result-item-isp .rating-container span[data-default="#1cbfff"]').should('have.length', rating);
+}
+
+module.exports = {
+    beginTests,
+    clickGoButton,
+    shouldHaveDownloadSpeed,
+    shouldHavePing,
+    shouldHaveUploadSpeed,
+    rateISP
+}
