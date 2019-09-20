62e1510fb66acbaf829bcdbd5298a96d00ef77ab
VishwasiniVageesh <55563829+VishwasiniVageesh@users.noreply.github.com>
2019-09-19 23:53:35 -0400

    Update lets-go-spec.js

--- cypress/integration/lets-go-spec.js
+++ cypress/integration/lets-go-spec.js
@@ -1,11 +1,25 @@
-const tests = require('../support/lets-go')
+const test = require('../support/lets-go')
 
 describe('Lets Begin', function() {
 
     it('Open browser to speedtest.net', function() {
-      tests.beginTests()
+      tests.beginTests() {
+          cy.visit('https://www.speedtest.net')
+      })
     })
-
+describe("Testing Login Functionality", function() {
+  it("Login ", function() {
+  cy.Contains('Log In').click()
+      cy.url().should('include', '/Register')
+  });
+});
+    
+ describe("Registering New User", function() {
+  it("Register ", function() {
+  cy.contains('Register'.click()
+      cy.url().should('include', '/Register')
+  });
+});
     // Performs a speed test
 
     // Verifies navigation etc...
